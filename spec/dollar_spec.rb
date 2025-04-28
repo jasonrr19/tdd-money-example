ec47f25cbcda4ab85c6dbe77283ddbd1fce2e930
Jason Rocha <jasonrr19@gmail.com>
2025-04-28 23:23:47 +0900

    part 2, almost done

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -3,7 +3,9 @@ require_relative '../lib/dollar'
 RSpec.describe Dollar do
   it 'multiplies correctly' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq(10)
+    product = five.times(2)
+    expect(product.amount).to eq(10)
+    product = five.times(3)
+    expect(product.amount).to eq(15)
   end
 end
