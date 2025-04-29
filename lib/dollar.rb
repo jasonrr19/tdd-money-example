0f29e7ebc7992db2f8ed20c58a64713f62942f9d
Jason Rocha <jasonrr19@gmail.com>
2025-04-29 16:21:27 +0900

    part 1 chpt 3 done

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -8,4 +8,9 @@ class Dollar
   def times(multiplier)
     Dollar.new(@amount * multiplier)
   end
+
+  def equals(object) #@amount is refering to what .equals method is being called on.
+    dollar = object
+    @amount == dollar.amount
+  end
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Dollar do
     product = five.times(3)
     expect(product.amount).to eq(15)
   end
+
+  it 'equates when amounts are the same' do
+    expect(Dollar.new(5).equals(Dollar.new(5))).to be true
+    expect(Dollar.new(5).equals(Dollar.new(6))).to be false
+  end
 end
